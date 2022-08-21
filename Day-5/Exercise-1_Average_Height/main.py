b6ef31416af5305ce7b5472bd8137aa0f2698509
Prem-Kumar16 <75419846+Prem-Kumar16@users.noreply.github.com>
2022-08-21 13:30:27 +0530

    Create main.py

---
+++ Day-5/Exercise-1_Average_Height/main.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+
+total_students = 0
+sum_of_height = 0
+
+for h in student_heights :
+    total_students += 1
+    sum_of_height += h
+
+average_height = round(sum_of_height / total_students)
+print(average_height)
