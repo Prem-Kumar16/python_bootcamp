ce45d3c51a80dc5d9cdd1503f4a93010c349e704
Prem-Kumar16 <75419846+Prem-Kumar16@users.noreply.github.com>
2022-09-14 11:09:07 +0530

    Add files via upload

--- Day 30/Update 2.0 - NATO Phonetic alphabet/main.py
+++ Day 30/Update 2.0 - NATO Phonetic alphabet/main.py
@@ -1,1 +1,19 @@
+import pandas
 
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+# print(phonetic_dict)
+
+letters_present = True
+
+while letters_present:
+
+    word = input("Enter a word: ").upper()
+
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+    else:
+        print(output_list)
+        letters_present = False
