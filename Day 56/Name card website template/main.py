d377a0bee8ea1e80cd84c1dd25de268d57503ed2
Prem-Kumar16 <75419846+Prem-Kumar16@users.noreply.github.com>
2022-10-21 14:48:48 +0530

    Add files via upload

--- Day 56/Name card website template/main.py
+++ Day 56/Name card website template/main.py
@@ -1,1 +1,14 @@
+from flask import Flask
+from flask import render_template
 
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    # Run the app in debug mode to auto-reload
+    app.run(debug=True)
